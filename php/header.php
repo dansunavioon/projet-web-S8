e62ed471c86e934694a3d1a6f6e19f011c476b11
Sofiane <sofiane.amokrane@isen-ouest.yncrea.fr>
2026-01-28 13:56:22 +0100

    test logo

--- php/header.php
+++ php/header.php
@@ -1,8 +1,9 @@
 <header class="site-header">
   <div class="header-inner">
     <a href="/php/accueil.php" class="brand">
-      <img src="/images/logo_buildux.png" alt="Logo du site">
     </a>
+          <img src="/images/logo_buildux.png" alt="Logo du site">
+
     <nav class="nav">
       <a href="/html/infos.html" class="nav-link">Contact us</a>
     </nav>
