4fae19445db9eb72b990348c820032e72afb390b
Sofiane <sofiane.amokrane@isen-ouest.yncrea.fr>
2026-01-28 16:45:38 +0100

    test login

--- php/Log_in.php
+++ php/Log_in.php
@@ -1,3 +1,3 @@
-<div style="position:fixed; top:10px; right:10px; z-index:9999; background:yellow; padding:10px;">
-  TEST LOGIN
-</div>
+<a href="../html/connexion.html" class="nav-link nav-link--login page-login-btn">
+  Log in / Sign in 
+</a>
