fb3a176a49ede57625eacf88126a9349a0839f4a
Sofiane <sofiane.amokrane@isen-ouest.yncrea.fr>
2026-01-28 14:28:21 +0100

    logo pas en dur

--- php/header.php
+++ php/header.php
@@ -1,16 +1,16 @@
 <header class="site-header">
   <div class="header-inner">
-    <a href="/Projet_buildux/projet-web-S8/php/acceuil.php" class="brand">
-      <img src="/Projet_buildux/projet-web-S8/images/logo_buildux.png" alt="Logo du site">
+    <a href="../php/acceuil.php" class="brand">
+      <img src="../images/logo_buildux.png" alt="Logo du site">
     </a>
     <nav class="nav">
-      <a href="/Projet_buildux/projet-web-S8/php/infos.php" class="nav-link">Contact us</a>
+      <a href="../php/infos.php" class="nav-link">Contact us</a>
     </nav>
     <button class="burger" aria-label="Menu">
       <span></span><span></span><span></span>
     </button>
   </div>
   <nav class="nav-mobile">
-    <a href="/Projet_buildux/projet-web-S8/php/infos.php" class="nav-link">Contact us</a>
+    <a href="../php/infos.php" class="nav-link">Contact us</a>
   </nav>
 </header>
