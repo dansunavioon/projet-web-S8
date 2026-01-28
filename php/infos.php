2f43cf515330404db23dae10077454ead8fd1150
Sofiane <sofiane.amokrane@isen-ouest.yncrea.fr>
2026-01-28 14:16:53 +0100

    ajout d'un test de contact us

---
+++ php/infos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>info de matteo : +33 7 67 57 65 26 tout pour 50 €</title>
+
+  <link rel="stylesheet" href="../css/header.css">
+  <link rel="stylesheet" href="../css/acceuil.css">
+</head>
+<body>
+
+ <?php include "../php/header.php"; ?>
+
+
+  <main>
+    <h1>Accueil</h1>
+  </main>
+
+  <script src="../js/header.js"></script>
+  <script src="../js/accueil.js"></script>
+
+</body>
+</html>
