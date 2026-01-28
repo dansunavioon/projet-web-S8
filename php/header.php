4c02edefba72deceb06000d3bee6dfd2f2acf0d0
Sofiane <sofiane.amokrane@isen-ouest.yncrea.fr>
2026-01-28 13:40:22 +0100

    correction nom

--- php/header.php
+++ php/header.php
@@ -1,7 +1,7 @@
 <header class="site-header">
   <div class="header-inner">
     <a href="/php/accueil.php" class="brand">
-      <img src="/image/logo.png" alt="Logo du site">
+      <img src="/image/logo_buildux.png" alt="Logo du site">
     </a>
     <nav class="nav">
       <a href="/html/infos.html" class="nav-link">Contact us</a>
