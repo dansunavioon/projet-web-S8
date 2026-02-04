e13675293d837702ba0b2c49f87bdbfce05104a5
enosolo <enosolo2003@gmail.com>
2026-02-04 11:47:35 +0100

    bouton login

--- php/Log_in.php
+++
@@ -1,3 +0,0 @@
-<a href="../html/connexion.html" class="nav-link nav-link--login page-login-btn">
-  Log in / Sign in 
-</a>
--- php/header.php
+++ php/header.php
@@ -1,12 +1,19 @@
 <header class="site-header">
   <div class="header-inner">
 
-    <a href="../php/acceuil.php" class="brand">
-      <img src="../images/logo_buildux.png" alt="Logo BuildUX">
-    </a>
+    <!-- LOGO + TEXTE -->
+    <div class="brand-group">
+      <a href="../php/acceuil.php" class="brand">
+        <img src="../images/logo_buildux.png" alt="Logo Buildux">
+      </a>
+      <span class="brand-text">Buildux the true project</span>
+    </div>
 
     <nav class="nav">
       <a href="../php/infos.php" class="nav-link">Contact us</a>
+      <a href="../php/log_in.php" class="nav-link nav-link--secondary">
+        Log in / Sign in
+      </a>
     </nav>
 
     <button class="burger" aria-label="Menu">
@@ -19,5 +26,8 @@
 
   <nav class="nav-mobile">
     <a href="../php/infos.php" class="nav-link">Contact us</a>
+    <a href="../php/log_in.php" class="nav-link nav-link--secondary">
+      Log in / Sign in
+    </a>
   </nav>
 </header>
