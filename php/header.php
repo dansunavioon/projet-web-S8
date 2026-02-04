3e942e5295c585ff3703d65c6c67e026255fb3bb
enosolo <enosolo2003@gmail.com>
2026-02-04 11:34:50 +0100

    test header

--- php/header.php
+++ php/header.php
@@ -1,15 +1,22 @@
 <header class="site-header">
   <div class="header-inner">
+
     <a href="../php/acceuil.php" class="brand">
-      <img src="../images/logo_buildux.png" alt="Logo du site">
+      <img src="../images/logo_buildux.png" alt="Logo BuildUX">
     </a>
+
     <nav class="nav">
       <a href="../php/infos.php" class="nav-link">Contact us</a>
     </nav>
+
     <button class="burger" aria-label="Menu">
-      <span></span><span></span><span></span>
+      <span></span>
+      <span></span>
+      <span></span>
     </button>
+
   </div>
+
   <nav class="nav-mobile">
     <a href="../php/infos.php" class="nav-link">Contact us</a>
   </nav>
