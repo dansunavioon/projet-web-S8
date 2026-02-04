b3aaa2f1f467dac91df30675d625a33d51d66164
enosolo <enosolo2003@gmail.com>
2026-02-04 13:42:30 +0100

    finistion de acceuil

--- php/acceuil.php
+++ php/acceuil.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
-  <title>Accueil </title>
+  <title>Accueil</title>
 
   <link rel="stylesheet" href="../css/acceuil.css">
   <link rel="stylesheet" href="../css/header.css">
@@ -12,8 +12,26 @@
   <?php include "../php/header.php"; ?>
   <?php include "../php/log_in.php"; ?>
 
-  <main>
-    <h1>Accueil </h1>
+  <main class="home">
+    <div class="home-layout">
+      <aside class="panel panel-left">
+        <div class="panel-title">Recherche avancée</div>
+      </aside>
+
+      <section class="panel panel-right">
+        <h1 class="search-title">Search for an internship</h1>
+
+        <div class="search-bar">
+          <div class="search-pill">Métier / Mots clés</div>
+          <div class="search-pill">Entreprise</div>
+          <div class="search-pill">Localisation</div>
+        </div>
+
+        <div class="results-box">
+          <div class="results-label">results</div>
+        </div>
+      </section>
+    </div>
   </main>
 
   <script src="../js/header.js"></script>
